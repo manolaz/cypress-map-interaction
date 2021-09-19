ce6ff0fd987568757bdf3fb44ad31eb1e34d6618
Tristan <trungupdate@gmail.com>
2021-09-19 09:06:21 +0700

    complete DRAG and DROP click

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ We are interested in automated tests (Cypress) to test the following actions:
 1. Click and drag to pan the map in canvas
 2. Mouse click 'Marina Bay Sands Singapore' from the map to select it.
 3. The rivers, parks, and beaches are correctly drawn on the canvas.
+
+====
+CYPRESS TEST SPEC file:
+==> cypress/integration/GoogleMapCanvas.spec.js
+
+======================
+Details actions:
+
+1/ VISITE the MAP 
+2/ Find CANVAS
+3/ Claculate it center
+4/ Click on the CAAVAS center
+5/ CLICK ,Keep pressing MOVE
+6/ drag to a little top up => (a quarter from TopUp , a quarter from LeftMostborder of the canvas ), LEAVE
+7/ Click on this point
+8/ find 'Marina Bay Sands Singapore' name content on the PANE 
+
--- cypress/integration/GoogleMapCanvas.spec.js
+++ cypress/integration/GoogleMapCanvas.spec.js
@@ -1,5 +1,5 @@
 describe("GOOGLE MAP app", () => {
-    it("get MAP canvas", () => {
+    it("get MAP canvas, to find hotel <Marina Bay Sands Singapore> ", () => {
       cy.visit("https://goo.gl/maps/xpE84cSewTG6RoqS7");
       cy.get("canvas").then($canvas =>{
         const canvasWidth = canvas.width();
@@ -8,8 +8,23 @@ describe("GOOGLE MAP app", () => {
         const canvasCenterHorizontal = canvasWidth/2;
         const canvasCenterVertical = canvasHeight/2;
 
-        cy.wrap($canvas).scrollIntoView();
+        const marinaX = canvasWidth/4;
+        const marinaY = canvasHeight/4;
+        
+        cy.wrap($canvas)
+        .scrollIntoView({ duration: 2000 })
+        .trigger("mousedown", canvasCenterVertical, canvasCenterHorizontal)
+        .trigger("mousemove", marinaX, marinaY)
+        .trigger('mouseup', { force: true })
+        .click()
+        ;      
+        // canvas.trigger("mousedown", canvasCenterVertical, canvasCenterHorizontal );
+        // id="pane"
       });
+
+      // .trigger('mouseup', 15, 40)
+
+      cy.get("#pane").find("Marina Bay Sands Singapore").should.be(visible);
         
     });
   });
